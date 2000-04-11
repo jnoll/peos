4baa0556f7c220d0a91b40c39beafc7482265520
nli <nli>
2000-04-11 03:45:23 +0000

    *** empty log message ***

--- vpml/pml/JMainFrame.java
+++ vpml/pml/JMainFrame.java
@@ -12,10 +12,9 @@ import javax.swing.application.datamodel.*;
 import pml.ui.*;
 
 /**
- * JMainFrame serves as the top level container frame for the PML UI and implements
- * many, non-view, default application behaviors such as menus, file IO, and so forth
- *
- *
+ * JMainFrame serves as the top level container frame for the PML UI and 
+ * implements many, non-view, default application behaviors such as menus, 
+ * file IO, and so forth
  * @author Matt Osminer (mosminer@ix.netcom.com)
  * @author Xing Wei
  */
@@ -25,29 +24,29 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
 {
     /**
      * Simple Delegation constructor that passes the main frame window title
-     * to the super class for proper display.  This constructor creates the split pane,
-     * the tree and graph view, and inserts all of the menus, toolbars and other little bells
-     * and whistles for the program.
-     *
+     * to the super class for proper display.  This constructor creates the 
+     * split pane, the tree and graph view, and inserts all of the menus,
+     * toolbars and other little bells and whistles for the program.
      * @param FrameTitle Text displayed in the mainframe window
-     * in future PML programs this will be the name of the process and it's associated
-     * filename
+     * in future PML programs this will be the name of the process and it's 
+     * associated filename
      */
 
     private JStatusArea statusArea = null;
     private Container contentPane = null;
     public JMainFrame()
     {
-        // Call the base class and pass the new frame title along.  In this case it's
-        // the applcation name - note that the title should change to reflect
-        // the currently open filename which is handled by the DataView
-        super( "" );
+        /* Call the base class and pass the new frame title along.In this case 
+         * it's the applcation name - note that the title should change to 
+         * reflect the currently open filename which is handled by the DataView
+         */
 
+        super( "" ); 
         // Set the default filename
         m_Filename = new String ( "Unnamed" );
 
-    		// Add the menu bar to which all menus will be attached
-		    JMenuBar MenuBar = new JMenuBar();
+	// Add the menu bar to which all menus will be attached
+        JMenuBar MenuBar = new JMenuBar();
         MenuBar.setBorder( new SoftBevelBorder( SoftBevelBorder.RAISED ) );
         setJMenuBar( MenuBar );
 
@@ -62,16 +61,18 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
 
         // Create a split pane so we can have the tree view on one side
         // and the Graph View on the right side
-        JSplitPane sp = new JSplitPane( JSplitPane.HORIZONTAL_SPLIT, jt, m_DataView );
+        JSplitPane sp = new JSplitPane( JSplitPane.HORIZONTAL_SPLIT, jt,
+                        m_DataView );
 
         // Now add the view to the content pane so we're off and running
         contentPane = getContentPane();
         contentPane.add( sp, BorderLayout.CENTER );
 
         // Setup the basics of the DataView
-// TO DO: Set the frame to the limits of the screen. Figure this out later!
-		Insets ins = getInsets();
-		setSize(ins.left + ins.right + 640,ins.top + ins.bottom + 480);
+        // TO DO: Set the frame to the limits of the screen. Figure this out
+        // later!
+	Insets ins = getInsets();
+	setSize(ins.left + ins.right + 640,ins.top + ins.bottom + 480);
 
         // Add a window event listener so we can catch window events
         addWindowListener( this );
@@ -92,7 +93,7 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
      * Private utility to build and set the mainframe title
      *
      */
-     private void setApplicationTitle()
+     private void setApplicationTitle() 
      {
         // Set the window's title so we include the filename
         setTitle( m_ApplicationTitle + " - " + m_Filename );
@@ -113,8 +114,8 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
             if ( op.showConfirmDialog( this,
                                        "Save Changes to Present Model?",
                                        "Save Changes",
-                                       JOptionPane.YES_NO_OPTION ) == JOptionPane.YES_OPTION )
-            {
+                                       JOptionPane.YES_NO_OPTION ) == 
+                                                 JOptionPane.YES_OPTION ) {
                 // Call the file save.  It will decide if the save chooser
                 // needs to be presented
                 fileSave( false );
@@ -140,7 +141,6 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
      * basic window closing event.  Of course we want to capture
      * the event and make sure the user has had a chance to save a document
      * to disk and tidy up any last minute program details
-     *
      * @param e The actual Window Closing event with closure information
      */
     public void windowClosing( WindowEvent e )
@@ -155,7 +155,6 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
     /**
      * Default window event handler - doesn't do anything but required
      * by WindowListener interface
-     *
      * @param e WindowEvent
      */
     public void windowClosed( WindowEvent e )
@@ -166,7 +165,6 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
     /**
      * Default window event handler - doesn't do anything but required
      * by WindowListener interface
-     *
      * @param e WindowEvent
      */
     public void windowDeactivated( WindowEvent e )
@@ -177,7 +175,6 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
     /**
      * Default window event handler - doesn't do anything but required
      * by WindowListener interface
-     *
      * @param e WindowEvent
      */
     public void windowDeiconified( WindowEvent e )
@@ -188,7 +185,6 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
     /**
      * Default window event handler - doesn't do anything but required
      * by WindowListener interface
-     *
      * @param e WindowEvent
      */
     public void windowIconified( WindowEvent e )
@@ -199,7 +195,6 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
     /**
      * Default window event handler - doesn't do anything but required
      * by WindowListener interface
-     *
      * @param e WindowEvent
      */
     public void windowOpened( WindowEvent e )
@@ -248,10 +243,10 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
     }
 
     /**                                  
-     * The file new method is called from the menu (actually through the PMLView)
-     * The File New command creates a new PML Process node and attaches it to this
-     * model.  From there the user may add or remove as desired.
-     *
+     * The file new method is called from the menu (actually through the 
+     * PMLView) The File New command creates a new PML Process node and 
+     * attaches it to this model. From there the user may add or remove as 
+     * desired.
      */
     public void fileNew()
     {
@@ -294,13 +289,14 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
         chooser.setDialogTitle( "Open" );
         
         // Implement a selection filters for PML models "PMD" files
-//TODO Setup the following with the proper filter - not available in my swing version right now
-//      chooser.setFileFilter( new ExtensionFileFilter( "pmd",
-//                                                      "PML Model Files" ) ); 
+        // TODO Setup the following with the proper filter - not available 
+        // in my swing version right now chooser.setFileFilter( new 
+        // ExtensionFileFilter( "pmd", "PML Model Files" ) );
 
-        String FileToOpen;                  // Storage for the file we are trying to open
-        JPMLDataModel DataModel = null;     // Storage for the DataModel we are supposedly reading
-        
+        String FileToOpen;     // Storage for the file we are trying to open
+        // Storage for the DataModel we are supposedly reading
+        JPMLDataModel DataModel = null;
+      
         // Show the save dialog. If the user hits OK then
         // set the filename to the selected filename
         // otherwise cancel was hit so leave this method
@@ -310,8 +306,7 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
             return;
         
         // Build up a file and dump the model object to it
-        try
-        {
+        try {
             FileInputStream PMLFile = new FileInputStream( FileToOpen );
             ObjectInputStream in = new ObjectInputStream( PMLFile );
             DataModel = (JPMLDataModel)in.readObject();
@@ -319,18 +314,14 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
             // Close the file
             PMLFile.close();
         
-        }
-        catch( IOException e )
-        {
+        } catch( IOException e ) {
             // Inform the user the file appears invalid and
             // return
             new JOptionPane().showMessageDialog( this, 
                                                  "File is not valid - " + e, 
                                                  "Error",
                                                  JOptionPane.ERROR_MESSAGE );
-        }
-        catch( ClassNotFoundException e )
-        {
+        } catch( ClassNotFoundException e ) {
             // Inform the user the file appears invalid and
             // return
             new JOptionPane().showMessageDialog( this, 
@@ -339,17 +330,18 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
                                                  JOptionPane.ERROR_MESSAGE );
         }
         
-        // if we get here and the data model is valid then we just loaded a valid filename
+        // if we get here and the data model is valid then we just loaded 
+        // a valid filename
         if ( DataModel == null )
             return;
         
         // First set the datamodel to the one we just loaded
         m_DataModel = DataModel;
-        
-        
+       
         // Next update the data model the View's are referring to
         m_DataView.setDataModel( m_DataModel );
-//TODO Insert this when the tree view is built        m_TreeView.setDataModel( m_DataModel );
+       //TODO Insert this when the tree view is built        
+       //m_TreeView.setDataModel( m_DataModel );
         
         // Save the filename as the newly loaded file
         m_Filename = FileToOpen;
@@ -361,9 +353,10 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
     
     /**
      * This method implements saving operations for the PMLModel so the user
-     * can interchange model files or restore previous sessions.  This method presents
-     * the SaveAs chooser dialog if the parameter flag is set, or if the model is unnamed
-     *
+     * can interchange model files or restore previous sessions.  This method 
+     * presents
+     * the SaveAs chooser dialog if the parameter flag is set, or if the model 
+     * is unnamed
      * @param SaveAs If true the method presents the file save as chooser
      *
      */
@@ -373,8 +366,7 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
         // The conditions for this dialog are - 
         // 1. input flag set to true
         // 2. the document filename is "Unnamed"
-        if ( SaveAs == true || m_Filename.equals( "Unnamed" ) )
-        {
+        if ( SaveAs == true || m_Filename.equals( "Unnamed" ) ) {
             // Create the chooser
             JFileChooser chooser = new JFileChooser(); 
             
@@ -399,8 +391,7 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
         }
         
         // Build up a file and dump the model object to it
-        try
-        {
+        try {
             FileOutputStream PMLFile = new FileOutputStream( m_Filename );
             ObjectOutputStream out = new ObjectOutputStream( PMLFile );
             out.writeObject( m_DataModel );
@@ -409,9 +400,7 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
             // Close the file
             PMLFile.close();
         
-        }
-        catch( IOException e )
-        {
+        } catch( IOException e ) {
             System.out.println( "[JMainFrame::fileSave] " + e );
         }
         
@@ -454,7 +443,7 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
         // Implement a selection filters for PML source "PML" files
 //TODO Setup the following with the proper filter - not available in my swing version right now
 //        chooser.setFileFilter( new ExtensionFileFilter( "pml",
-//                                                        "PML Source Files" ) ); 
+//                                                       "PML Source Files" ) ); 
 
         // Show the save dialog. If the user hits OK then
         // set the filename to the selected filename
@@ -465,16 +454,14 @@ public class JMainFrame extends JFrame implements java.io.Serializable,
             return;
             
 //TODO Implement file overwrite verification here
-        // We got a valid filename here so open a file and generate the PML into then close
-        try
-        {
-            BufferedWriter file = new BufferedWriter( new FileWriter( PMLFilename ) );
+        // We got a valid filename here so open a file and generate the PML into        // then close
+        try {
+            BufferedWriter file = new BufferedWriter( new FileWriter( 
+                                                           PMLFilename ) );
             file.write( m_DataModel.toPML() );
             file.flush();
             file.close();
-        }
-        catch ( IOException e )
-        {
+        } catch ( IOException e ) {
             // Inform the user the file appears invalid and
             // return
             new JOptionPane().showMessageDialog( this, 
