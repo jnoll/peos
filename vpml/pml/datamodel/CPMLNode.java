7b3a8201e5a93cb9ab42c9b8ab2e73d871635ffe
nli <nli>
2000-04-11 03:53:02 +0000

    *** empty log message ***

--- vpml/pml/datamodel/CPMLNode.java
+++ vpml/pml/datamodel/CPMLNode.java
@@ -107,31 +107,33 @@ abstract public class CPMLNode extends CGraphNode
         // Basically we're going to read in a word at a time.  If the
         // total line length is greater than 80 (std screen width) then 
         // start a newline until we've completed all the comment conversion
-        for ( int WordStartIndex = 0; WordStartIndex < m_Comments.length(); WordStartIndex++)
-        {
+        for ( int WordStartIndex = 0; WordStartIndex < m_Comments.length(); 
+                                      WordStartIndex++) {
             // First locate the length of the next word looking for spaces here
-            while ( m_Comments.charAt( WordStartIndex + WordLength++ ) != ' ' &&
-                    WordLength + WordStartIndex < m_Comments.length() );
+            while ( m_Comments.charAt( WordStartIndex + WordLength++ ) != ' ' 
+                   && WordLength + WordStartIndex < m_Comments.length() );
 
-            // If we've exceeded the end of the source string then decrement the WordLen
-            // by one so we don't choke trying to remove the substring
+            // If we've exceeded the end of the source string then decrement
+            // the WordLen by one so we don't choke trying to remove the 
+            // substring
             if ( WordLength >= m_Comments.length() )
                 WordLength--;
             
-            // Now we've got either the end of the Comments, or the end of a word
-            // if we've got a word then check the line length plus the word length
-            // if we exceed 80 characters then start a new line
-            if ( CurrentLineCount + WordLength > 79 )
-            {
+            // Now we've got either the end of the Comments, or the end of a 
+            // word if we've got a word then check the line length plus the 
+            // word length if we exceed 80 characters then start a new line
+            if ( CurrentLineCount + WordLength > 79 ) {
                 // Add a newline and a standard comment string
                 Comments += "\n" + Whitespace + "// ";
                 CurrentLineCount = 4 + Whitespace.length();
             }
 
             // Get the word from the local comments string
-            Word = m_Comments.substring( WordStartIndex, WordStartIndex + WordLength );
+            Word = m_Comments.substring( WordStartIndex, WordStartIndex +
+                                                               WordLength );
             
-            // Add the word to the current PML formatted comment string we're building
+            // Add the word to the current PML formatted comment string 
+            // we're building
             Comments += Word;
             
             // Add the word length to the word index and chars processed
@@ -150,8 +152,8 @@ abstract public class CPMLNode extends CGraphNode
     }
 
     /** 
-     * Method to call for outputting the children of this node into the PML string
-     * This just recurses the children which recurses their children etc
+     * Method to call for outputting the children of this node into the PML 
+     * string This just recurses the children which recurses their children etc
      * @param pmlString destination string for PML code
      * @param SpaceCardinality The number of spaces to insert on each newline
      */
@@ -164,10 +166,11 @@ abstract public class CPMLNode extends CGraphNode
         if ( ChildrenAllowed() == false )
             return RetVal;
             
-        // Otherwise recurse the children of this node.  Add 3 to the space cardinality so
-        // all children are indented by three space in scope
+        // Otherwise recurse the children of this node.  Add 3 to the space
+        // cardinality so all children are indented by three space in scope
         for ( int ChildNum = 0; ChildNum < NumberOfChildren(); ChildNum++ )
-            RetVal += ((CPMLNode)GetAChild( ChildNum )).toPMLString( pmlString, SpaceCardinality + 3 );
+            RetVal += ((CPMLNode)GetAChild( ChildNum )).toPMLString( pmlString,
+                                                         SpaceCardinality + 3 );
             
         return RetVal;
     }
@@ -176,7 +179,6 @@ abstract public class CPMLNode extends CGraphNode
      * This routine builds a string of empty spaces and is used as a utility
      * routine.  This is really clunky for now.  Perhaps later somebody can 
      * clean it up
-     *
      * @param length 
      */
      public String buildWhitespace( int length )
