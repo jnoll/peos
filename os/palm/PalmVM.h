bdf0d938cfad650d445e2fa66f725d3403598647
peos2002 <peos2002>
2003-01-27 17:07:49 +0000

    VM-Engine interface hard-coded

---
+++ os/palm/PalmVM.h
@@ -0,0 +1,14 @@
+/*
+ * File: PalmVM.h
+ * Author: Mark Mastroieni
+ * Date: 11/15/02
+ */
+
+typedef enum {
+  COMPLETE,
+  SYSCALL,
+  PROC_ERROR,
+  INTERN_ERROR
+} VM_state;
+
+VM_state execute();
