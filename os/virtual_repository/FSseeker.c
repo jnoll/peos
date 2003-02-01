a28c6c03d64a15432db8318db72ca888c2a84b64
msaeed <msaeed>
2003-02-01 00:27:06 +0000

    url for file system

--- os/virtual_repository/FSseeker.c
+++ os/virtual_repository/FSseeker.c
@@ -16,6 +16,7 @@
 
 static char *path = NULL ;
 query *ftwQuery ;
+char *ftwValue;
 
 queryList* FSqueryTool( queryList *listpointer )
 {
@@ -23,7 +24,7 @@ queryList* FSqueryTool( queryList *listpointer )
 	
 	int done ;
 	FILE *configFile ;
-   	char line[100], *word ;
+   	char line[100], *word, *rep_type, *temp_value ;
    	struct stat statBuffer ;
    	queryList *theQueries ;
    	
@@ -52,25 +53,62 @@ queryList* FSqueryTool( queryList *listpointer )
   	}
 
 	theQueries = listpointer ;
+		
+	
+	
 	while( theQueries != NULL )
 	{
-		if( strchr( theQueries -> oneQuery -> myClauses[0].value, '/' ) != NULL )
+		
+		temp_value =  (char *) malloc ( strlen( theQueries -> oneQuery -> myClauses[0].value ) * sizeof( char ) ) ;
+		strcpy(temp_value, theQueries -> oneQuery -> myClauses[0].value ) ;
+		rep_type = strtok( temp_value , ":" ) ;
+		
+		
+		if( strncmp( "file", rep_type, 4 ) == 0 )
 		{
-			if( stat( theQueries -> oneQuery -> myClauses[0].value, &statBuffer ) == 0 )
+			puts(theQueries -> oneQuery -> myClauses[0].value);
+			puts("repository type is file");
+			
+					
+			strcpy(temp_value, strpbrk(theQueries -> oneQuery -> myClauses[0].value,"/"));
+				
+			
+			
+			if( strchr(temp_value+2, '/' ) != NULL )
 			{
-				theQueries -> oneQuery -> results = 
-					addResultItem ( theQueries -> oneQuery -> results, 
-							theQueries -> oneQuery -> myClauses[0].value ) ;
-			    	theQueries -> oneQuery -> numFound++ ;
+				
+				
+				if( stat( temp_value+1, &statBuffer ) == 0 )
+				{
+					puts("opening by stat");
+					theQueries -> oneQuery -> results = 
+						addResultItem ( theQueries -> oneQuery -> results, 
+								theQueries -> oneQuery -> myClauses[0].value ) ;
+			    		theQueries -> oneQuery -> numFound++ ;
+				}
 			}
+			else
+			{
+				ftwQuery = theQueries -> oneQuery ;
+				ftwValue = (char *) malloc ( strlen( temp_value+1 ) * sizeof( char ) );
+				strcpy(ftwValue, temp_value+2 ) ;
+				
+				
+			
+				ftw( path, getFile, 5 ) ;
+				
+				free(ftwValue);
+				
+			}		
+			theQueries = ( queryList* ) theQueries -> link ;
+			
+			
+			
+			
 		}
-		else
-		{
-			ftwQuery = theQueries -> oneQuery ;
-			ftw( path, getFile, 5 ) ;
-		}		
-		theQueries = ( queryList* ) theQueries -> link ;
-	}
+		free(temp_value);
+		
+	} 
 	return listpointer ;	
 }
      
@@ -90,9 +128,13 @@ int getFile( const char *filename, const struct stat *statptr, int flag )
 
     				strcpy( target, filename + index + 1 ) ;
 	    									
-				if( strncmp( ftwQuery -> myClauses[0].value, target,
-					     strlen( ftwQuery -> myClauses[0].value ) ) == 0 )
+				//if( strncmp( ftwQuery -> myClauses[0].value, target,
+				//	     strlen( ftwQuery -> myClauses[0].value ) ) == 0 )
+				
+				if( strncmp( ftwValue, target,
+					     strlen( ftwValue) ) == 0 )
 				{
+					puts("opening by ftw");
 					ftwQuery -> results = addResultItem ( ftwQuery -> results, filename );
 				    	ftwQuery -> numFound++ ;
 				}
