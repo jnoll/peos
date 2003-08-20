c7228be2061f3e8b923aaa87cc0eada885b48173
wchu <wchu>
2003-08-20 21:01:41 +0000

    *** empty log message ***

--- ui/web2/cgi-bin/create.c
+++ ui/web2/cgi-bin/create.c
@@ -1,187 +1,82 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "events.h"
-#include "vm.h"
 #include "process_table.h"
-
-
-void load_actions();
-void create_process(char *model);
-void print_action(peos_action_t *action);
-
+#include "vm.h"
 
 int main()
 {
-  char *input, *model_name, cont_proc;
+  int i, j;
+  char **result = (char **) peos_list_models();
+  char **result2;
 
+  load_proc_table("proc_table.dat");
+  result2 = peos_list_instances(result2);
+  save_proc_table("proc_table.dat");
 
-  printf("%s%c%c\n","Content-Type:text/html;charset=iso-8859-1",13,10);
+  printf("%s%c%c\n\n","Content-Type:text/html;charset=iso-8859-1",13,10);
   printf("<html>\n");
   printf("<head>\n");
+  printf("<style type=\"text/css\">\n");
+  printf("<!--\n");
+  printf(".myTable {\n");
+  printf("background-image:url(../images/bg.jpg);\n");
+  printf("background-repeat:no-repeat;\n");
+  printf("background-position:left top; }\n");
+  printf("-->\n");
+  printf("</style>\n");
   printf("</head>\n");
 
-  printf("<body>\n");
-  input = getenv("QUERY_STRING");
-
-  if(strcmp(input,"") == 0)
-  {}
-  else if(sscanf(input,"model_list=%s", model_name) != 1)
-  {
-    printf("<P>Error! Error in passing variable.\n");
-    exit(1);
+  printf("<body class=\"myTable\">\n");
+  printf("<br><br><br><br><br><br><br><br><br><br>\n");
+  printf("<table width=\"820px\" align=\"center\" border=\"0\" celspacing=\"0\" cellpadding=\"0\">\n");
+  printf("<tr>\n");
+  printf("<td width=\"50%\">\n");
+  printf(" <form name=\"model_form\" action=\"http://linux.students.engr.scu.edu/~wchu/cgi-bin/create.cgi\">\n");
+  printf("<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n");
+  printf("<tr>\n");
+  printf("<td width=\"40%\">\n");
+  printf("&nbsp;&nbsp&nbsp&nbsp\n");
+  printf("<select width=\"160\" style=\"width: 160px\" name=\"model_list\" size=\"10\">\n");
+  printf("<option selected> Select Model </option>\n");
+  for(j=0; result && result[j]; j++){
+    printf("<option value=\"%s\">%s</option>\n",result[j], result[j]);
   }
-  else if(model_name == NULL)
-  {
-    printf("<P>Error! Error in passing variable.\n");
-    exit(1);
-  }
-  else if(strcmp(model_name,"cont") == 0)
-  {}
-  else
-  {
-    load_proc_table("proc_table.dat");
-    create_process(model_name);
-    save_proc_table("proc_table.dat");
-  }
-  
+  printf("</select>\n");
+  printf("</td>\n");
+  printf("<td width=\"60%\" valign=\"bottom\">\n");
+  printf("&nbsp;&nbsp;\n");
+  printf("<input type=\"submit\" value=\"Create Model\">\n");
+  printf("</td>\n");
+  printf("</tr>\n");
+  printf("</table>\n");
+  printf("</form>\n");
+  printf("</td>\n");
 
-  printf("<P><br><br><br></P>\n"); 
-  load_actions();
+  printf("<td>\n");
+  printf("<form action=\"http://linux.students.engr.scu.edu/~wchu/cgi-bin/create.cgi\">\n");
+  printf("<table width=\"100%\" align=\"center\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n");
+  printf("<td width=\"40%\">\n");
+  printf("<select width=\"160\" style=\"width: 160px\" size=\"10\" name=\"model_list\">\n");
+  printf("<option value=\"cont\" selected>Active Processes</option>\n");
+  for(i=0; i <= PEOS_MAX_PID; i++){
+    printf("<option value=\"cont\">%d&nbsp;&nbsp;&nbsp;%s</option>\n", i, result2[i]);
+  }
+  printf("</select>\n"); 
+  printf("</td>\n");
+  printf("<td width=\"60%\" valign=\"bottom\">\n");
+  printf("&nbsp;&nbsp;\n");
+  printf("<input type=\"submit\" value=\"Continue Process\">\n");
+  printf("</td>\n");
+  printf("</table>\n");
+  printf("</form>\n");
+  printf("</td>\n");
+  printf("</tr>\n");
+  printf("</table>\n");
 
-  printf("<script language=\"JavaScript\">\n");
-  printf("<!--\n");
-  printf("function enable_run()\n");
-  printf("{document.formMain.run.disabled=false;\n");
-  printf("document.formMain.done.disabled=true;\n");
-  printf("document.formMain.suspend.disabled=true;\n");
-  printf("document.formMain.resume.disabled=true;}\n");
-  printf("function enable_done()\n");
-  printf("{document.formMain.done.disabled=false;\n");
-  printf("document.formMain.suspend.disabled=false;\n");
-  printf("document.formMain.run.disabled=true;\n");
-  printf("document.formMain.resume.disabled=true;}\n");
-  printf("function enable_resume()\n");
-  printf("{document.formMain.resume.disabled=false;\n");
-  printf("document.formMain.run.disabled=true;\n");
-  printf("document.formMain.suspend.disabled=true;\n");
-  printf("document.formMain.done.disabled=true;}\n");
-  printf("//-->\n");
-  printf("</script>\n");
+
   printf("</body>\n");
-  printf("</html>\n");
-
-
+	  
   return 0;
-}  
-
-void load_actions()
-{
-  int i;
-  peos_action_t **alist;
-
-  printf("<form name=\"formMain\" action=\"http://linux.students.engr.scu.edu/~wchu/cgi-bin/run.cgi\">\n");
-
-  printf("<table width=\"100\%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n");
-  printf("<tr>\n");
-  printf("Active\n");
-  printf("</tr>\n");
-  printf("<tr>\n");
-  printf("<td width=\"33%\">\n");
-  printf("<select width=\"160\" style=\"width: 160px\" name=\"action\" size=\"2\"
-		        onchange=\"enable_done()\">\n");
-  load_proc_table("proc_table.dat");
-  alist = peos_list_actions(ACT_RUN);
-  if (alist && alist[0]){
-    for(i=0; alist[i]; i++){
-      print_action(alist[i]);
-    } 
-  }
-  save_proc_table("proc_table.dat");
-  printf("</select>\n");
-  printf("<input type=\"submit\" size=\"20\" name=\"done\" value=\"Done\" disabled>");
-  printf("<input type=\"submit\" size=\"20\" name=\"suspend\" value=\"Suspend\" disabled>");
-  printf("</td>\n");
-  printf("<td align=\"center\">\n");
-  printf("<p><textarea width=\"720\" style=\"width: 720px\" rows=\"4\" disabled>\n");
-  printf("</textarea></p>\n");
-  printf("</td>\n");
-  printf("</tr>\n");
-  printf("</table>\n");
-
-  printf("<table width=\"100\%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n");
-  printf("<br><br><br>\n");
-  printf("<tr>\n");
-  printf("<td width=\"33%\">\n");
-  printf("Ready\n");
-  printf("</td>\n");
-  printf("<td width=\"67%\">\n");
-  printf("Suspended\n");
-  printf("</td>\n");
-  printf("</tr>\n");
-
-  printf("<tr>\n");
-
-  printf("<td width=\"33%\">\n"); 
-  printf("<select width=\"160\" style=\"width: 160px\" name=\"action\" size=\"10\" 
-	  onclick=\"enable_run()\">\n"); 
-  load_proc_table("proc_table.dat");
-  alist = peos_list_actions(ACT_READY);
-  if (alist && alist[0]){
-    for(i=0; alist[i]; i++){
-      print_action(alist[i]);
-    }
-  }
-  save_proc_table("proc_table.dat");
-  printf("</select>\n");
-  printf("<input type=\"submit\" size=\"20\" name=\"run\" value=\"Run\" disabled 
-          onclick=\"run_process()\">");
-  printf("</td>\n");
-
-  printf("<td width=\"34%\">\n");
-  printf("<select width=\"160\" style=\"width: 160px\" name=\"action\" size=\"10\"
-          onchange=\"enable_resume()\">\n");
-  load_proc_table("proc_table.dat");
-  alist = peos_list_actions(ACT_SUSPEND);
-  if (alist && alist[0]){
-    for(i=0; alist[i]; i++){
-      print_action(alist[i]);
-    }
-  }
-  save_proc_table("proc_table.dat");
-  printf("</select>\n");
-  printf("<input type=\"submit\" name=\"resume\" value=\"Resume\" disabled>");
-  printf("</td>\n");
-  printf("</tr>\n");
-  printf("</table>\n");
-  printf("</form>\n");
-
-  printf("<br><br>\n");
-  printf("<table width=\"100\%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n");
-  printf("<tr>\n");
-  printf("<td align=\"left\">\n");
-  printf("<br>\n");
-  printf("<form action=\"http://linux.students.engr.scu.edu/~wchu/cgi-bin/test.cgi\">\n");
-  printf("<input type=\"submit\" value=\"Create Process\">\n"); 
-  printf("</td>\n");
-  printf("</tr>\n");
-  printf("</form>\n");
-  printf("</table>\n");
 }
 
-void create_process(char *model)
-{
-  int pid;
-
-  load_proc_table("proc_table.dat");
-  if ((pid = peos_run(model, 0)) < 0) {
-     printf("<P>%d</P>\n", pid);
-     printf("<P>Couldn't create %s</P>\n", model);
-  }
-  save_proc_table("proc_table.dat");
-}
-
-void print_action(peos_action_t *action)
-{
-  printf("<option value=\"%d %s\">%d %s</option>\n"
-         , action->pid, action->name, action->pid, action->name);
-}
---
+++ ui/web2/cgi-bin/main.c
@@ -0,0 +1,117 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "events.h"
+#include "vm.h"
+#include "process_table.h"
+
+void print_action(peos_action_t *action, char *state);
+
+
+int main()
+{
+  int i;
+  peos_action_t **alist;
+
+  printf("%s%c%c\n","Content-Type:text/html;charset=iso-8859-1",13,10);
+
+  printf("<html>\n");
+  printf("<head>\n");
+  printf("<style type=\"text/css\">\n");
+  printf("<!--\n");
+  printf(".myTable {\n");
+  printf("background-image:url(../images/bg.jpg);\n");
+  printf("background-repeat:no-repeat;\n");
+  printf("background-position:left top; }\n");
+  printf("-->\n");
+  printf("</style>\n");
+  printf("</head>\n");
+  printf("<body class=\"myTable\" link=\"#FFCC66\" vlink=\"#FFCC66\" alink=\"#FFCC66\">\n");
+  printf("<br><br><br><br><br>\n");
+  printf("<table width=\"950px\">\n");
+  printf("<tr>\n");
+  printf("<td width=\"100\" rowspan=\"3\">\n");
+  printf("</td>\n");
+  printf("<td width=\"210\">\n");
+  printf("<font size=\"5\"<b><u>Status</u></b></font>\n"); 
+  printf("</td>\n");
+  printf("<td width=\"210\">\n");
+  printf("<font size=\"5\"<b>Task</b>\n"); 
+  printf("</td>\n");
+  printf("<td width=\"210\">\n");
+  printf("<font size=\"5\"><b>Resource(s)</b>\n"); 
+  printf("</td>\n");
+  printf("<td width=\"220\">\n");
+  printf("<font size=\"5\"><b>Description</b>\n"); 
+  printf("</td>\n");
+  printf("</tr>\n");
+  printf("<tr>\n");
+  printf("<td width=\"210px\">\n");
+  load_proc_table("proc_table.dat");
+  alist = peos_list_actions(ACT_RUN);
+  if(alist && alist[0]){
+    for(i=0; alist[i]; i++){
+      printf("active<br>\n");
+    }
+  }  
+  alist = peos_list_actions(ACT_SUSPEND);
+  if(alist && alist[0]){
+    for(i=0; alist[i]; i++){
+      printf("suspended<br>\n"); 
+    }
+  }
+  alist = peos_list_actions(ACT_READY);
+  if(alist && alist[0]){
+    for(i=0; alist[i]; i++){
+      printf("ready<br>\n"); 
+    }
+  }
+  save_proc_table("proc_table.dat");
+  printf("</td>\n");
+  printf("<td width=\"210\">\n");
+  load_proc_table("proc_table.dat");
+  alist = peos_list_actions(ACT_RUN);
+  if(alist && alist[0]){
+    for(i=0; alist[i]; i++){
+      print_action(alist[i], "active");
+    }
+  }  
+  alist = peos_list_actions(ACT_SUSPEND);
+  if(alist && alist[0]){
+    for(i=0; alist[i]; i++){
+      print_action(alist[i], "suspend");
+    }
+  }  
+  alist = peos_list_actions(ACT_READY);
+  if(alist && alist[0]){
+    for(i=0; alist[i]; i++){
+      print_action(alist[i], "ready");
+    }
+  }  
+  save_proc_table("proc_table.dat");    
+  printf("</td>\n");
+  printf("<td width=\"210\">\n");
+  printf("</td>\n");
+  printf("<td width=\"220\">\n");
+  printf("</td>\n");
+  printf("</tr>\n");
+  printf("<tr>\n");
+  printf("<td>\n");
+  printf("<br><br><br><br><br>\n");
+  printf("<a href=\"http://linux.students.engr.scu.edu/~wchu/cgi-bin/create.cgi\">\n"); 
+  printf("<img src=\"../images/create_button.jpg\" width=\"80\" height=\"25\" border=\"0\">\n");
+  printf("</a>\n");
+  printf("</td>\n");
+  printf("</tr>\n");
+  printf("</table>\n");
+
+  printf("</body>\n");
+  printf("</html>\n");
+
+  return 0;
+}
+
+void print_action(peos_action_t *action, char *state)
+{
+  printf("<a href=\"http://linux.students.engr.scu.edu/~wchu/cgi-bin/action_page.cgi?%d+%s=%s\"> %s<br>\n", action->pid, action->name, state, action->name);
+  printf("</a>");
+}
