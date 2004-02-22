b20f0db60849bf64178014f82f6e004e66ca77ad
jshah1 <jshah1>
2004-02-22 04:04:27 +0000

    deleted winston's code files

--- ui/web2/cgi-bin/proc_graph.h
+++
@@ -1,5 +0,0 @@
-//#ifndef _PROC_GRAPH_H
-//#define _PROC_GRAPH_H
-extern Graph make_graph(char *file);
-extern void makedot(Node n, char *filename, char *action, char *state, int pid);
-//#endif
--- ui/web2/readme.pml
+++
@@ -1,9 +0,0 @@
-process peos_howto {
-  iteration {
-    action README {
-      script { "Welcome to PEOS.
-	<p>This system maintains a list of tasks to perform."
-      }
-    }
-  }
-}
