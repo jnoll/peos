aaf82e11ef9e4876cb4cda330dc871a4f2a1b03c
jshah1 <jshah1>
2004-02-24 03:33:13 +0000

    create_testtable to create a test table for the tests

---
+++ ui/web2/cgi-bin/create_testtable.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "kernel/events.h"
+#include "kernel/action.h"
+#include "kernel/process_table.h"
+
+
+int main()
+{
+
+    /* create a test process table */	
+    int pid;
+    int i;
+    char *model = "test_process.pml";
+    peos_resource_t *resources;
+    int num_resources;
+    char *process_filename = "test.dat"; 
+
+
+    peos_set_process_table_file(process_filename);
+
+    resources = (peos_resource_t *) peos_get_resource_list(model,&num_resources);
+    
+    for(i=0; i < num_resources; i++) {
+        strcpy(resources[i].value,"$$");
+    }	
+    pid = peos_run(model, resources, num_resources);
+	
+    exit(0);
+
+}	
