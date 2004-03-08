1a7a5dbefb350a0e6fdd088212d13f0be03ccb04
jnoll <jnoll>
2004-03-08 09:49:58 +0000

    Revised to invoke lynx without -mime_header option.  The version of lynx on linux101.dc.engr.scu.edu doesn't allow this option to be used with the lynxcgi protocol, thus I had to remove the grep tests for redirects.

--- ui/web2/cgi-bin/test_action_page_fail.sh
+++ ui/web2/cgi-bin/test_action_page_fail.sh
@@ -3,18 +3,18 @@ echo -n "$0..."
 PROC_TABLE=dfTSHEvITlEm2.dat
 
 #echo "CREATE PROCESS"
-lynx -dump -mime_header "lynxcgi:create_process.cgi?model=commit_changes.pml&process_filename=${PROC_TABLE}" | grep "Location: action_list.cgi" > /dev/null || echo "create process FAILED"
+lynx -source  "lynxcgi:create_process.cgi?model=commit_changes.pml&process_filename=${PROC_TABLE}" 
 
 #echo "RUN ACTION"
-lynx -dump -mime_header "lynxcgi:action_page.cgi?pid=0&act_name=update_workspace&process_filename=${PROC_TABLE}" | grep '<h1>update_workspace</h1>' > /dev/null || echo "run action FAILED"
+lynx -source  "lynxcgi:action_page.cgi?pid=0&act_name=update_workspace&process_filename=${PROC_TABLE}" | grep '<h1>update_workspace</h1>' > /dev/null || echo "run action FAILED"
 
 #echo "BIND RESOURCES"
-lynx -mime_header "lynxcgi:bind_resources.cgi?pid=0&act_name=update_workspace&resource_type=requires&workspace=/usr/local/src/httpunit&process_filename=${PROC_TABLE}" | grep "Location: action_page.cgi" > /dev/null || echo "bind resources FAILED"
+lynx -source "lynxcgi:bind_resources.cgi?pid=0&act_name=update_workspace&resource_type=requires&workspace=/usr/local/src/httpunit&process_filename=${PROC_TABLE}"
 
 [ -f ${PROC_TABLE} ] || echo "FAILURE: process table ${PROC_TABLE} missing"
 
 #echo "LIST ACTIONS"
-lynx -mime_header "lynxcgi:action_page.cgi?process_filename=${PROC_TABLE}&pid=0&act_name=update_workspace" | grep '<h1>update_workspace</h1>' > /dev/null || echo "list actions FAILED"
+lynx -source "lynxcgi:action_page.cgi?process_filename=${PROC_TABLE}&pid=0&act_name=update_workspace" | grep '<h1>update_workspace</h1>' > /dev/null || echo "list actions FAILED"
 
 [ -f ${PROC_TABLE} ] || echo "FAILURE: process table ${PROC_TABLE} missing"
 
