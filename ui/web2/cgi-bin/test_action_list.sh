d202a1ca0291d29d0a97ac514f84720921cdf7a6
jshah1 <jshah1>
2004-03-12 21:07:57 +0000

    the test for action_list.cgi

--- ui/web2/cgi-bin/test_action_list.sh
+++ ui/web2/cgi-bin/test_action_list.sh
@@ -88,4 +88,90 @@ rm output
 rm test.dat
 
 
+#test whether process state gets updated after a resource becomes available
+export QUERY_STRING="process_filename=test.dat&act_name=test2&pid=0&resource_type=requires&r3=r3val"
+export REQUEST_METHOD=GET
+                                                                               
+create_testtable
+change_resource_bindings.cgi > /dev/null
+
+export QUERY_STRING="process_filename=test.dat&start=false"
+touch r3val
+
+action_list.cgi > output
+
+
+if !(grep '<h1>Action List</h1>' output > /dev/null)
+then
+  echo
+  echo Failed Action List header.
+  echo
+fi
+if !(grep 'Process Id' output > /dev/null)
+then
+  echo
+  echo Failed Process Id.
+  echo
+fi
+if !(grep 'Action Name' output > /dev/null)
+then
+  echo
+  echo Failed Action Name.
+  echo
+fi
+if !(grep 'State' output > /dev/null)
+then
+  echo
+  echo Failed State.
+  echo
+fi
+if !(grep 'Check to Indicate Done' output > /dev/null)
+then
+  echo
+  echo Failed checkbox header.
+  echo
+fi
+if !(grep 'test1' output > /dev/null)
+then
+  echo
+  echo Failed test1.
+  echo
+fi
+
+if !(grep 'test2' output > /dev/null)
+then
+  echo
+  echo Failed test2.
+  echo
+fi
+if !(grep 'READY' output > /dev/null)
+then
+  echo
+  echo Failed test1 not ready.
+  echo
+fi
+if !(grep 'AVAILABLE' output > /dev/null)
+then
+  echo
+  echo Failed test2 not Available.
+  echo
+fi
+if !(grep '0\$test1' output > /dev/null)
+then
+  echo
+  echo Failed checkbox value 1.
+  echo
+fi
+
+if !(grep '0\$test2' output > /dev/null)
+then
+  echo
+  echo Failed checkbox value 2.
+  echo
+fi
+
+rm output
+rm r3val
+rm test.dat
+
 echo "done"
