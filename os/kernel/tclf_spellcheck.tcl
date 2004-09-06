6ce854fd4f7cae2de76dae9815717052fbba0bff
ppozdnya <ppozdnya>
2004-09-06 07:49:46 +0000

    Made changes to Tcl functions

--- os/kernel/tclf_spellcheck.tcl
+++ os/kernel/tclf_spellcheck.tcl
@@ -1,8 +1,6 @@
 proc spellcheck { filename } {
-
-        #set c [expr sqrt($a * $a + $b * $b)]
-#	set s "spellchecking file: $filename"
 	puts stdout "spellcheck arguments: $filename"
-	return 44
-
+	puts stdout [ expr { [exec spell $filename] } ]
+	puts stdout "------------------------------"
+	expr { [exec spell $filename] }
 }
--- os/kernel/tclf_timestamp.tcl
+++ os/kernel/tclf_timestamp.tcl
@@ -1,28 +1,6 @@
-
 proc timestamp { path1 path2 } {
-	puts stdout "timestamp arguments: $path1 $path2"
-        #set path1 mymodule.mod
-	#set path2 myexec.exe
-        #file stat $path1 stat1
-        #file stat $path2 stat2
-        #return [expr $stat1(mtime) > $stat2(mtime)]
-	#$stat1(ino) == $stat2(ino) && \
-         #               $stat1(dev) == $stat2(dev)
-	 return 55
+        puts stdout "[tclf_timestamp.tcl] Timestamp arguments: $path1 $path2"
+        file stat $path1 stat1
+        file stat $path2 stat2
+        expr { $stat1(mtime) > $stat2(mtime) }
 }
- 
-
-
-#proc timestamp { file1 file2 } {
-       # set c [expr sqrt($a * $a + $b * $b)]
-  # puts stdout $file1
-   #puts stdout $file2
-#        file stat $file1 stat1
-  #   file stat $file2 stat2
-   #  if { $stat1(mtime) > $stat2(mtime)} {
-#       return 1
-#     } else {
- #      return 0
-#   }
-
-#}
