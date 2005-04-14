243a770543fc7010caeab16bbb79ec59d6504468
sbeeby <sbeeby>
2005-04-14 06:42:40 +0000

    Added test cases for load_process, PalmPEOS ][ now successfully loads processes. Test case loads timesheet twice and verifies its name is entered in the process table.

--- os/palm/PalmCUnit/TestParser.c
+++ os/palm/PalmCUnit/TestParser.c
@@ -15,7 +15,7 @@
 #include "segments.h"
 #include <action.h>
 #include <resources.h>
-
+#include <graph.h>
 
 extern void initialize_suite();
 extern void terminate_suite();
@@ -66,29 +66,38 @@ static void test_peos_list_models(){
 	char ** ret;
 	Int16 dbc;
 	dbc = DmNumDatabases(0);
-	//ASSERT_INT16_EQUAL(5, dbc);
 	ret=peos_list_models(); 
-	if (ret == NULL)
-		ASSERT_STR_EQUAL("yo", "YOU");
+	ASSERT_MSG("peos_list_models() returns null", ret!=NULL);
+	
 	ASSERT_STR_EQUAL("AddressDB", ret[0]);
 }
 
 static void test_load()
 {
-	UInt32 i;
-	int num_resources=-1;
-	peos_resource_t *resources;
+	char **processes;
 	char *model;
-	model = strdup("simple");
-	resources = get_resource_list(model, &num_resources);
+	peos_resource_t *resource;
+	int x;
+	int num_resources=-1;
+	model = strdup("timesheet");
 	ASSERT_STR_EQUAL(model, find_model_file(model));
+	processes=peos_list_instances();
+	ASSERT_STR_EQUAL("", processes[0]);
+
+	resource=get_resource_list(model, &num_resources); 
+	ASSERT_INT32_EQUAL(1, num_resources);
+
+	load_proc_table();
+
+	x = peos_create_instance(model, resource, num_resources);
+	ASSERT_INT32_EQUAL(0, x);
+	x = peos_create_instance(model, resource, num_resources);
+	ASSERT_INT32_EQUAL(1, x);
+	processes=peos_list_instances();
+	ASSERT_STR_EQUAL("timesheet", processes[0]);
+	ASSERT_STR_EQUAL("timesheet", processes[1]);
 	free(model);
-	ASSERT_INT32_EQUAL(0, num_resources);
-/*	for(i=0; i< num_resources; i++)
-	{
-		strcpy(resources[i].value, "$$");
-	}*/
-//	ASSERT_INT32_EQUAL(0, peos_run(model, resources, num_resources));
+
 }
 static void test_fread()
 {
@@ -102,7 +111,7 @@ static void test_fread()
 }
 static void test_makegraph()
 {
-	Graph *g=makegraph("simple");
+	Graph g=makegraph("simple");
 	ASSERT_MSG("G is null", g != NULL);
 
 }
@@ -114,7 +123,6 @@ static void test_ListCreate()
 
 static void test_TreeCreate()
 {
-	//printf("Testing Tree Create-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 	peos = TreeCreate(TreeCreate(NULL,NULL,"Steve",1),
 			TreeCreate(NULL, NULL, "Ark",2),
 				"John",0);
@@ -129,7 +137,6 @@ static void test_TreeDelete()
 }
 static void test_ListAdd()
 {
-//	ListInsert(x, 0, "Steve");
 	steve = "Steve";
 	ASSERT_STR_EQUAL("Steve",ListPush(x,steve));
 	ASSERT_STR_EQUAL("Steve", (char *) ListGet(x));
@@ -163,7 +170,6 @@ void initialize_suite() {
   TestGroup* group;
 
   group = add_test_group("CUnit", initializeCUnitGroup, terminateCUnitGroup, initializeCUnitTest, terminateCUnitTest);
-//  add_test_case(group, "MakeGraph", test_makegraph);
   add_test_case(group, "strdup", test_Strdup);
   add_test_case(group, "ListCreate", test_ListCreate);
   add_test_case(group, "ListAdd", test_ListAdd);
